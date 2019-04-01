087c288d03a5b3ae7a9c4d073c3efe05948342f8
522248352 <522248352@qq.com>
2019-04-01 16:27:45 +0800

    python-图片上传优化

--- Api_TestCase/picture_upload.py
+++ Api_TestCase/picture_upload.py
@@ -2,23 +2,23 @@
 import requests
 import json
 
-#图片上传
+# 图片上传
 hosts = "https://sandbox-api-onerway.ronhan.com"
 
 url = hosts + "/upload/image.htm"
 
 parameter = {"partnerId":170776145556545536}
 
-#图片
-#rb	以二进制格式打开一个文件用于只读。文件指针将会放在文件的开头。这是默认模式。一般用于非文本文件如图片等。
-files_png = {"image":("278.png",open("D:\\278.png","rb"),"image/png",{})}
-files_jpg = {"image":("315.jpg",open("D:\\315.jpg","rb"),"image/jpeg",{})}
+# 图片
+# rb	以二进制格式打开一个文件用于只读。文件指针将会放在文件的开头。这是默认模式。一般用于非文本文件如图片等。
+files_png = {"image": ("278.png", open("D:\\278.png", "rb"), "image/png", {})}
+files_jpg = {"image": ("315.jpg", open("D:\\315.jpg", "rb"), "image/jpeg", {})}
 
-#方法一
-results = requests.post(url,data=parameter,files=files_png)
+# 方法一
+results = requests.post(url, data=parameter, files=files_png)
 print(results.text)
 
-#方法二
+# 方法二
 # results = requests.request("POST",url,data=parameter,files=files_png)
 # print(results.text)
-open("D:\\278.png","rb").close()
+open("D:\\278.png", "rb").close()
