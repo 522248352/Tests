0be09200554b0ad0168ef5141f22a385db9ca32f
522248352 <522248352@qq.com>
2019-04-01 15:47:51 +0800

    python-添加全局变量备注

--- Api_TestCase/global_parameter.py
+++ Api_TestCase/global_parameter.py
@@ -4,5 +4,5 @@
 
 HOSTS = "https://sandbox-api-onerway.ronhan.com"   # api接口host地址
 PARTNERID = "170776145556545536"  # 代理商编号
-MERNO_HAVA_USERID = "171805655366242304"      # 商户编号
-MERNO_NO_USERID = "276944710478757888"        # 商户编号
+MERNO_HAVA_USERID = "171805655366242304"      # 商户编号-有用户ID
+MERNO_NO_USERID = "276944710478757888"        # 商户编号-无用户ID
