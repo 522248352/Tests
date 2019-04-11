76bb7d70090c4a6d6583106578f2ea7f9bb8f6b3
522248352 <522248352@qq.com>
2019-04-11 16:11:10 +0800

    csv文件和调用方法

--- Api_TestCase/csv_opera.py
+++ Api_TestCase/csv_opera.py
@@ -1,15 +1,12 @@
 # coding=utf-8
 import csv
 
-csv_file = csv.reader(open("D:\csv.csv", "r"))
+csv_file = csv.reader(open("D:\eclipse-workspace\Tests\Api_TestCase\csv.csv", "r"))
 # csv_file_dic = csv.DictReader(fieldnames="D:\csv.csv")
 
-with open("D:\csv.csv", "r") as filename:
+with open("D:\eclipse-workspace\Tests\Api_TestCase\csv.csv", "r") as filename:
     csv_re = csv.DictReader(filename)
     for row in csv_re:
         print(row)
-        data ={}
-        data["a"] =row["T1"]
-        data["b"] =row["T2"]
-        data["c"] =row["T3"]
-        print(data)
+        print(type(row))
+
