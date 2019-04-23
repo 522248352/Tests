85332f8de0d8d27d7c524967ca868e7f6a8e4065
522248352 <522248352@qq.com>
2019-04-23 15:33:00 +0800

    ceds

---
+++ UI_AutoTest/sandbox_test.py
@@ -0,0 +1,20 @@
+# coding=utf-8
+from selenium import webdriver
+import time
+
+driver = webdriver.Chrome()
+driver.get("https://sandbox-manage-onerway.ronhan.com/")
+driver.maximize_window()
+driver.find_element_by_name("username").send_keys("admin")
+driver.find_element_by_name("password").send_keys("123123zxc")
+driver.find_element_by_xpath('//*[@id="loginForm"]/div[3]/button').click()
+time.sleep(3)
+
+driver.find_element_by_class_name("title").click()
+driver.find_element_by_id("list0101").click()
+time.sleep(3)
+
+rows = driver.find_elements_by_tag_name("tr")
+print(len(rows))
+
+driver.quit()
